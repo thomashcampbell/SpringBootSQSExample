5b496d4a60b468d206882cbe44e51cd8b3ca0aea
Thomas Campbell <thomas@aotscc.com>
2023-03-11 22:21:11 -0800

    Fixed prodcuer Removed some extraneous code

--- src/main/java/com/aotscc/sqs/config/SpringCloudSQSConfig.java
+++ src/main/java/com/aotscc/sqs/config/SpringCloudSQSConfig.java
@@ -6,15 +6,12 @@ import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.core.env.ResourceIdResolver;
-import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import java.util.Collections;
+
 @Configuration
 @EnableSqs
 public class SpringCloudSQSConfig {
@@ -27,14 +24,9 @@ public class SpringCloudSQSConfig {
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${springbootsqsexample.pollLength}")
-    private int pollLength;
-
-    @Value("${springbootsqsexample.maxMessages}")
-    private int maxMessages;
-
     // Mapping Converter for serializing / deserializing the messages produced / consumed
-    private MappingJackson2MessageConverter createMappingJackson2MessageConverter() {
+    @Bean
+    public MappingJackson2MessageConverter createMappingJackson2MessageConverter() {
         // Configure ObjectMapper
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -47,40 +39,13 @@ public class SpringCloudSQSConfig {
         return messageConverter;
     }
 
-    // Insert the Mapping Message Converter into the QueueMessageTemplate
-    // (I don't know what's going on with ResourceIdResolver error in Intellij)
     @Bean
-    public QueueMessagingTemplate myMessagingTemplate(ResourceIdResolver resourceIdResolver) {
-        return new QueueMessagingTemplate(amazonSQSAsync(), resourceIdResolver, createMappingJackson2MessageConverter() );
+    public QueueMessagingTemplate queueMessagingTemplate() {
+        return new QueueMessagingTemplate(amazonSQSAsync());
     }
-
-    // Create the SimpleMessageListenerContainerFactory with the Mapping Message Converter
-    @Bean
-    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory() {
-        SimpleMessageListenerContainerFactory factory =
-                new SimpleMessageListenerContainerFactory();
-
-        factory.setAmazonSqs(amazonSQSAsync());
-
-        factory.setAutoStartup(true);
-
-        // Maximum Number Of Messages to Consume at One Time
-        factory.setMaxNumberOfMessages(maxMessages);
-
-        // Poll Timeout
-        factory.setWaitTimeOut(pollLength);
-
-        factory.setQueueMessageHandler(
-                new QueueMessageHandler(
-                        Collections.singletonList(createMappingJackson2MessageConverter())));
-
-        return factory;
-    }
-
 
     // Amazon SQS Async
-    // This has to be public because of AOP
-    public AmazonSQSAsync amazonSQSAsync() {
+    private AmazonSQSAsync amazonSQSAsync() {
 
         AmazonSQSAsyncClientBuilder amazonSQSAsyncClientBuilder =
                 AmazonSQSAsyncClientBuilder.standard();
--- src/main/java/com/aotscc/sqs/producer/Producer.java
+++ src/main/java/com/aotscc/sqs/producer/Producer.java
@@ -1,13 +1,11 @@
 package com.aotscc.sqs.producer;
 
 import com.aotscc.sqs.dto.Student;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,27 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/produce")
 public class Producer {
 
-	private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
 
-	@Autowired
-	private QueueMessagingTemplate queueMessagingTemplate;
+    @Autowired
+    private QueueMessagingTemplate queueMessagingTemplate;
 
-	@Value("${cloud.aws.end-point.uri}")
-	private String endPoint;
+    @Value("${cloud.aws.end-point.uri}")
+    private String endPoint;
 
-	// Post a Message of Type Student to the queue
-	@PostMapping("/message")
-	public Student sendMessage(@RequestBody Student student) {
+    // Post a Message of Type Student to the queue
+    @PostMapping("/message")
+    public Student sendMessage(@RequestBody Student student) {
 
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			String jsonString = mapper.writeValueAsString(student);
-			queueMessagingTemplate.send(endPoint, MessageBuilder.withPayload(jsonString).build());
-			logger.info("Message sent successfully  " + jsonString);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return student;
-	}
+        try {
+            queueMessagingTemplate.convertAndSend(endPoint, student);
+
+            logger.info("Message sent successfully  " + student);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return student;
+    }
 
 }
